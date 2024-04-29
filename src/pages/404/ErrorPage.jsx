eac77882b0fec9accf7dd4193a75a2aa4b6ef5e6
Duc-Lee Banh <duclee.banh@gmail.com>
2024-04-29 21:39:13 +0200

    Created Error 404 page

---
+++ src/pages/404/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import Nav from "../../components/Nav";
+
+function ErrorPage() {
+    return <>
+        <Nav />
+        <div>
+            404 Page Not Found
+        </div>
+    </>
+}
+
+export default ErrorPage;
