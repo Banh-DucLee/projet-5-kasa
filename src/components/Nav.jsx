c161480d45bdde456ff5aeb0564a3ddf5e97ab53
Duc-Lee Banh <duclee.banh@gmail.com>
2024-04-29 23:02:58 +0200

    Completed Nav component structure

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,17 @@
 import { NavLink } from'react-router-dom';
 
+import '../styles/Nav.scss';
+
+import logoColor from '../assets/images/logo.png';
+
 function Nav() {
-    return <nav>
-        <NavLink to="/">Accueil</NavLink>
-        <NavLink to="/about">A Propos</NavLink>
-    </nav>
+    return <header>
+        <img src={logoColor} alt='Logo de Kasa'></img>
+        <nav>
+            <NavLink to="/">Accueil</NavLink>
+            <NavLink to="/about">A Propos</NavLink>
+        </nav>
+    </header>
 }
 
 export default Nav;
