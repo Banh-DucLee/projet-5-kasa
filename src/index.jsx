da88439a6c3646a7110afc7e1604cec947e3871e
Duc-Lee Banh <duclee.banh@gmail.com>
2024-04-29 23:00:27 +0200

    Imported index.scss

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import Router from './components/Router';
 
+import './styles/index.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
