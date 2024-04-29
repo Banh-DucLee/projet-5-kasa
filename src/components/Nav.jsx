261953edd09b1a27d9264b9803482990d9fef250
Duc-Lee Banh <duclee.banh@gmail.com>
2024-04-29 21:38:37 +0200

    Created Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from'react-router-dom';
+
+function Nav() {
+    return <nav>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="/about">A Propos</NavLink>
+    </nav>
+}
+
+export default Nav;
