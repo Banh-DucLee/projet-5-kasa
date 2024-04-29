0bdbc077078e48e8762b78cc5fa7bfcf6d1b2f80
Duc-Lee Banh <duclee.banh@gmail.com>
2024-04-29 23:03:30 +0200

    Added Footer component after Outlet to Root component

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 import Nav from "./Nav";
+import Footer from "./Footer";
 
 function Root() {
     return <>
         <Nav />
         <Outlet />
+        <Footer />
     </>
 }
 
