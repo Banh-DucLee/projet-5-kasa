ef7f451dc45e818ebfde2a872fdcec23fa88b443
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-03 11:08:47 +0200

    Added Banner component to HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,13 @@
+import Banner from '../../components/Banner';
+
+import "../../styles/HomePage.scss";
+
+import backgroundImage from '../../assets/images/Paysage1.jpg';
+
 function HomePage() {
-    return <div>
-        Home Page
-    </div>
+    return <main>
+        <Banner imageUrl={backgroundImage} text="Chez vous,<br /> partout et ailleurs" opacity="0.6"/>
+    </main>
 }
 
 export default HomePage;
