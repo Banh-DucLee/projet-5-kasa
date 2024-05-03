febbd261772e4374a78b96781702cceab81fe6ec
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-03 11:26:38 +0200

    Added Banner component to AboutPage

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,7 +1,13 @@
+import Banner from "../../components/Banner";
+
+import "../../styles/AboutPage.scss";
+
+import backgroundImage from "../../assets/images/Paysage2.jpg";
+
 function AboutPage() {
-    return <div>
-        About Page
-    </div>
+    return <main>
+        <Banner imageUrl={backgroundImage} opacity="0.3"/>
+    </main>
 }
 
 export default AboutPage;
