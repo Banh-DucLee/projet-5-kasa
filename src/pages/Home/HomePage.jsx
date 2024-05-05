d1905367f5488b55fbc519af09187ce75b19cad4
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-05 20:39:09 +0200

    Fully functionnal HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,20 @@
 import Banner from '../../components/Banner';
+import Card from '../../components/Card';
 
 import "../../styles/HomePage.scss";
 
+import data from '../../assets/json/logements.json';
 import backgroundImage from '../../assets/images/Paysage1.jpg';
 
 function HomePage() {
+    console.log(data);
     return <main>
         <Banner imageUrl={backgroundImage} text="Chez vous,<br /> partout et ailleurs" opacity="0.6"/>
+        <div className="cards-container">
+            {data.map((housing) => (
+                <Card key={housing.id} housing={housing} />
+            ))}
+        </div>
     </main>
 }
 
