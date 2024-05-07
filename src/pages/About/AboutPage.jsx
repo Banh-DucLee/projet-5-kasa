898e16f1c4e050192ee5712a2b5ef557b13a4488
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-07 09:19:57 +0200

    Added Collapse component to AboutPage

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,12 +1,19 @@
 import Banner from "../../components/Banner";
+import Collapse from "../../components/Collapse";
 
 import "../../styles/AboutPage.scss";
 
+import data from "../../assets/json/about.json"
 import backgroundImage from "../../assets/images/Paysage2.jpg";
 
 function AboutPage() {
     return <main>
         <Banner imageUrl={backgroundImage} opacity="0.3"/>
+        <div className="collapse-container">
+            {data.map((item) => (
+                <Collapse key={item.title} data={item} />
+            ))}
+        </div>
     </main>
 }
 
